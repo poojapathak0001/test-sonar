4798a544dc0310020eee94f130109b5ad02d39ff
Pooja <poojapathak0001@gmail.com>
2018-05-17 06:26:20 +0530

    initial commit

---
+++ src/main/java/org/demo/demo/MyUnit.java
@@ -0,0 +1,16 @@
+package org.demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyUnit 
+{
+	    public String concatenate(String one, String two){
+	        return one + two;
+	    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
